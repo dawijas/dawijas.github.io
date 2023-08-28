70f9fc0e3f12936fe6cfe571ebbe51ab8e271ee6
dawijas <vertexy10@gmail.com>
2023-08-28 13:56:02 +0200

    Astrada

---
+++ AstradaStudio/js/script.js
@@ -0,0 +1,8 @@
+
+
+const mobileMenu = document.querySelector('.mobile-menu');
+const menu = document.querySelector('.menu');
+
+mobileMenu.addEventListener('click', () => {
+  menu.classList.toggle('active');
+});
